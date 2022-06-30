84a01f948eff53ba631890be15886a59e57fe05b
goaferlx <58623934+goaferlx@users.noreply.github.com>
2022-06-30 11:26:48 +0100

    basic db migrations

---
+++ sql/migrate.go
@@ -0,0 +1,58 @@
+package sql
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/golang-migrate/migrate"
+	"github.com/golang-migrate/migrate/database/mysql"
+)
+
+func (db *DB) MigrateUp() error {
+	m, err := db.migrator("file://../sql")
+	if err != nil {
+		return fmt.Errorf("sql: creating migrator: %w", err)
+	}
+
+	if err := m.Up(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
+		return fmt.Errorf("sql: migrating up: %w", err)
+	}
+	return nil
+}
+
+func (db *DB) MigrateDown() error {
+	m, err := db.migrator("file://../sql")
+	if err != nil {
+		return fmt.Errorf("sql: creating migrator: %w", err)
+	}
+
+	if err := m.Down(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
+		return fmt.Errorf("sql: migrating down: %w", err)
+	}
+
+	return nil
+}
+
+func (db *DB) DestructiveReset() error {
+	m, err := db.migrator("file://../sql")
+	if err != nil {
+		return fmt.Errorf("sql: creating migrator: %w", err)
+	}
+
+	if err := m.Down(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
+		return fmt.Errorf("sql: migrating down: %w", err)
+	}
+	if err := m.Up(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
+		return fmt.Errorf("sql: migrating up: %w", err)
+	}
+	return nil
+}
+
+func (db *DB) migrator(filePath string) (*migrate.Migrate, error) {
+	instance, err := mysql.WithInstance(db.DB, &mysql.Config{})
+	if err != nil {
+		return nil, fmt.Errorf("sql: creating db instance: %w", err)
+	}
+	return migrate.NewWithDatabaseInstance(filePath, "mysql", instance)
+
+}
