1fbc8e1d100b4611aa77de88d9e8192793d27bed
goaferlx <58623934+goaferlx@users.noreply.github.com>
2023-01-28 15:53:56 +0000

    removed logging from sql package and some housekeeping

--- sql/sql.go
+++ sql/sql.go
@@ -7,14 +7,23 @@ import (
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/goaferlx/go-core/log"
+)
+
+// Default values for configuring the DB connection pool.  Values taken from
+// https://www.alexedwards.net/blog/configuring-sqldb - accessed 2023/01/28.
+const (
+	DefaultMaxOpenConns int           = 25
+	DefaultMaxIdleConns int           = 5
+	DefaultMaxLifetime  time.Duration = 5 * time.Minute
+	DefaultMaxIdleTime  time.Duration = 5 * time.Minute
 )
 
 type DB struct {
 	*sql.DB
-	path string
+	path string // path to migration files.
 }
 
+// BeginTx wraps the sql.BeginTx and sets a tx time.
 func (db *DB) BeginTx(ctx context.Context, opts *sql.TxOptions) (*Tx, error) {
 	tx, err := db.DB.BeginTx(ctx, nil)
 	if err != nil {
@@ -31,12 +40,16 @@ type Tx struct {
 	*sql.Tx
 }
 
+// Now returns the time that the Tx started at.
 func (tx *Tx) Now() time.Time {
-	return tx.now
+	return tx.now.UTC()
 }
 
 var ErrNoRows = sql.ErrNoRows
 
+// Open is a convenience function that wraps sql.Open to establish a connection to the DB
+// and verifies the connection, in one step, as well as setting sensible default values
+// for the connection pool.
 func Open(cfg Config) (*DB, error) {
 	db, err := sql.Open(cfg.Dialect, cfg.DSN())
 	if err != nil {
@@ -46,12 +59,11 @@ func Open(cfg Config) (*DB, error) {
 		return nil, fmt.Errorf("sql: %w", err)
 	}
 
-	log.WithFields(log.Fields{
-		"dialect": cfg.Dialect,
-		"dbname":  cfg.DBName,
-		"host":    cfg.Host,
-		"port":    cfg.Port,
-	}).Info("Connected to database")
+	// TODO: Change these to values from config.
+	db.SetMaxOpenConns(DefaultMaxOpenConns)
+	db.SetMaxIdleConns(DefaultMaxIdleConns)
+	db.SetConnMaxLifetime(DefaultMaxLifetime)
+	db.SetConnMaxIdleTime(DefaultMaxIdleTime)
 
 	return &DB{DB: db}, nil
 }
