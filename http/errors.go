459c8767ddbc3a5f18c93be90daac379ac0bc1ab
goaferlx <58623934+goaferlx@users.noreply.github.com>
2023-01-28 10:17:02 +0000

    changes to errors to keep the linter happy

--- http/errors.go
+++ http/errors.go
@@ -3,7 +3,7 @@ package http
 import "errors"
 
 // ErrNotFound is a standard error to return with 404 status code.
-var ErrNotFound error = errors.New("Hello, is it me you're looking for? Because the page you requested doesn't exist.")
+var ErrNotFound error = errors.New("http: Hello, is it me you're looking for? Because the page you requested doesn't exist")
 
 // ErrNotAllowed is a standard error to return with 405 status.
 var ErrNotAllowed error = errors.New("http: we don't do that here")
