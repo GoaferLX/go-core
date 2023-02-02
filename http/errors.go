47096ab15a827a8956affb41daa05a586e7289b2
goaferlx <58623934+goaferlx@users.noreply.github.com>
2023-02-02 20:52:24 +0000

    formatting

--- http/errors.go
+++ http/errors.go
@@ -2,14 +2,16 @@ package http
 
 import "errors"
 
-// ErrNotFound is a standard error to return with 404 status code.
-var ErrNotFound error = errors.New("http: Hello, is it me you're looking for? Because the page you requested doesn't exist")
+var (
+	// ErrNotFound is a standard error to return with 404 status code.
+	ErrNotFound error = errors.New("http: Hello, is it me you're looking for? Because the page you requested doesn't exist")
 
-// ErrNotAllowed is a standard error to return with 405 status.
-var ErrNotAllowed error = errors.New("http: we don't do that here")
+	// ErrNotAllowed is a standard error to return with 405 status.
+	ErrNotAllowed error = errors.New("http: we don't do that here")
 
-// ErrInvalidRequest is a standard error to return with 400 status code.
-var ErrInvalidRequest error = errors.New("http: invalid request or payload")
+	// ErrInvalidRequest is a standard error to return with 400 status code.
+	ErrInvalidRequest error = errors.New("http: invalid request or payload")
 
-// ErrUnsupportedMedia is a standard error to return with 415 status code.
-var ErrUnsupportedMedia error = errors.New("http: unsupported media type")
+	// ErrUnsupportedMedia is a standard error to return with 415 status code.
+	ErrUnsupportedMedia error = errors.New("http: unsupported media type")
+)
